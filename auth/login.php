cc8f24c40af03df77182018a9209aff885e579bb
justojaramillo <justojaramillo1991@gmail.com>
2025-02-09 20:59:33 -0500

    login feature finished

--- auth/login.php
+++ auth/login.php
@@ -2,6 +2,30 @@
 define("PATH","http://realestate.test");
 require_once("../html.php");
 require_once("../config/config.php");
+
+if (isset($_POST['submit'])) {
+  if (empty($_POST['email']) || empty($_POST['password'])) {
+    echo "<script>alert('Some inputs are empty');</script>";
+  }else {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $login = $conn->prepare("SELECT * FROM user WHERE email=:email");
+    $login->execute([":email"=>$email]);
+    $user = $login->fetch(PDO::FETCH_ASSOC);
+    if ($login->rowCount()>0) {
+      if (password_verify($password,$user['password'])) {
+        echo "Logged";
+      }else {
+        echo "<script>alert('Email or Password is wrong');</script>";
+      }
+    }else {
+      echo "<script>alert('Email is wrong');</script>";
+    }
+  }
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,17 +57,17 @@ require_once("../config/config.php");
         <div class="row">
           <div class="col-md-12" data-aos="fade-up" data-aos-delay="100">
             <h3 class="h4 text-black widget-title mb-3">Login</h3>
-            <form action="" class="form-contact-agent">
+            <form action="login.php" method="post" class="form-contact-agent">
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" class="form-control">
+                <input type="email" name="email" id="email" class="form-control">
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" id="password" class="form-control">
+                <input type="password" name="password" id="password" class="form-control">
             </div>
             <div class="form-group">
-                <input type="submit" id="phone" class="btn btn-primary" value="Login">
+                <input type="submit" name="submit" id="phone" class="btn btn-primary" value="Login">
             </div>
             </form>
           </div>
