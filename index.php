cc6f88bc397b9ebc2ac80febddb8b13c969bb477
justojaramillo <justojaramillo1991@gmail.com>
2025-02-22 23:09:07 -0500

    properties feature in index part 1

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 @session_start();
 define("PATH","http://realestate.test");
 require_once("html.php");
+require_once("config/config.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,33 +18,29 @@ require_once("html.php");
 
     <?php require_once("includes/navbar.php"); ?>
 
+    <?php
+    $select = $conn->prepare("SELECT * FROM property");
+    $select->execute();
+
+    $properties = $select->fetchAll(PDO::FETCH_OBJ);
+
+    ?>
     <div class="slide-one-item home-slider owl-carousel">
 
-      <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
-            <div class="col-md-10">
-              <span class="d-inline-block bg-success text-white px-3 mb-3 property-offer-type rounded">For Rent</span>
-              <h1 class="mb-2">871 Crenshaw Blvd</h1>
-              <p class="mb-5"><strong class="h2 text-success font-weight-bold">$2,250,500</strong></p>
-              <p><a href="#" class="btn btn-white btn-outline-white py-3 px-5 rounded-0 btn-2">See Details</a></p>
+      <?php foreach($properties as $property): ?>
+        <div class="site-blocks-cover overlay" style="background-image: url(images/<?= $property->image ?>);" data-aos="fade" data-stellar-background-ratio="0.5">
+          <div class="container">
+            <div class="row align-items-center justify-content-center text-center">
+              <div class="col-md-10">
+                <span class="d-inline-block bg-success text-white px-3 mb-3 property-offer-type rounded">For <?= $property->type ?></span>
+                <h1 class="mb-2"><?= $property->name ?></h1>
+                <p class="mb-5"><strong class="h2 text-success font-weight-bold">$ <?= number_format($property->price,2,'.',',') ?></strong></p>
+                <p><a href="property-details.php?id=<?= $property->property_id ?>" class="btn btn-white btn-outline-white py-3 px-5 rounded-0 btn-2">See Details</a></p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>  
-
-      <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
-            <div class="col-md-10">
-              <span class="d-inline-block bg-danger text-white px-3 mb-3 property-offer-type rounded">For Sale</span>
-              <h1 class="mb-2">625 S. Berendo St</h1>
-              <p class="mb-5"><strong class="h2 text-success font-weight-bold">$1,000,500</strong></p>
-              <p><a href="#" class="btn btn-white btn-outline-white py-3 px-5 rounded-0 btn-2">See Details</a></p>
-            </div>
-          </div>
-        </div>
-      </div>  
+        </div>  
+      <?php endforeach; ?>
 
     </div>
 
